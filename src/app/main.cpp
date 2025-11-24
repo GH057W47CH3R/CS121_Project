43a22d0f51a0c15a99e4653c489fa9fefed0fc8e
ethan <eam0040@uah.edu>
2025-11-23 21:49:06 -0600

    added try catch in main

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -11,12 +11,16 @@ int main(int argc, char *argv[]) {
     return 1;
   }
   fs::path p = argv[1];
-
   AppState app_state;
-  app_state.load_from_file(p);
-  // do stuff
-  // TODO: Integrate UI Loop
+  try {
+    app_state.load_from_file(p);
+    // do stuff
+    // TODO: Integrate UI Loop
 
-  app_state.save_to_file(p);
+    app_state.save_to_file(p);
+  } catch (const std::exception &e) {
+    std::cerr << "Error: " << e.what() << std::endl;
+  }
+
   return 0;
 }
