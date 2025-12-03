d6b0d35189c83ed32886d2ac3ff5f81982d5883e
Kaleb <kchank02@gmail.com>
2025-12-02 20:42:59 -0600

    Adding functionality to write out and read in IDs

--- src/core/record.hpp
+++ src/core/record.hpp
@@ -17,15 +17,18 @@ struct Record {
 
 //Serialize the record by writing the raw bytes to an output stream
 void serialize(std::ostream& out) const{
+  out.write(reinterpret_cast<const char*>(&id), sizeof(id));
   out.write(name, max_name_bytes);
   out.write(address, max_address_bytes);
   out.write(phone, max_phone_bytes);
 }
 //Deserialize the record by reading the raw bytes from an input stream
 void deserialize(std::istream& in) {
+  in.read(reinterpret_cast<char*>(&id), sizeof(id));
   in.read(name, max_name_bytes);
   in.read(address, max_address_bytes);
   in.read(phone, max_phone_bytes);
 }
 
 };
+
