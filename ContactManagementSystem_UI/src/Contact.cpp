fa9e2dac4cd825cf07edb06f7f9739c0cbf3aabd
GH057W47CH3R <154854592+GH057W47CH3R@users.noreply.github.com>
2025-12-03 14:49:20 -0600

    Delete ContactManagementSystem_UI directory
    Going to edit what's needed into the app_state

--- ContactManagementSystem_UI/src/Contact.cpp
+++
@@ -1,8 +0,0 @@
-#include "../include/Contact.h"
-
-Contact::Contact(const std::string& name,
-    const std::string& phoneNumber,
-    const std::string& email,
-    const std::string& address)
-    : name(name), phoneNumber(phoneNumber), email(email), address(address) {
-}
