0712df49278bafc5111f77b33c1d7382b14086fb
Kaleb <kchank02@gmail.com>
2025-12-02 20:02:59 -0600

    Removed num_records()

--- src/core/app_state.hpp
+++ src/core/app_state.hpp
@@ -3,6 +3,7 @@
 #include "ptr_array.hpp"
 #include "record_array.hpp"
 #include <filesystem>
+#include <fstream>
 
 namespace fs = std::filesystem;
 
@@ -27,15 +28,12 @@ private:
   RecordArray records_state_;
 
 public:
-  void load_from_file(const fs::path &);
-  void save_to_file(const fs::path &);
+  void load_from_file(const fs::path &p);
+  void save_to_file(const fs::path &p);
   void add_record_to_state(Record);
   void delete_record_from_state(std::uint32_t);
-  std::uint32_t num_records();
-  const Record &record_at(std::uint32_t i) const { return records_state_[i]; }
   RecordView select(const Predicate &pred) {
-    return view_match<const Record>(records_state_.data_, records_state_.size_,
-                                    pred);
+    return view_match<const Record>(records_state_.data_, records_state_.size_, pred);
   }
   MutableRecordView select_mut(const Predicate &pred) {
     return view_match<Record>(records_state_.data_, records_state_.size_, pred);
