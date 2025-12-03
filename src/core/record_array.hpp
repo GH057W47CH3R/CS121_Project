f9d540cd7db572a46e3b667ea80e3788378b1702
Kaleb <kchank02@gmail.com>
2025-12-02 20:05:35 -0600

    Added comments as well as both serialization functions

--- src/core/record_array.hpp
+++ src/core/record_array.hpp
@@ -2,23 +2,29 @@
 #include "record.hpp"
 #include <cstdint>
 
+//This is a dynamically resizable array of the Record objects
+//Has member functions for resizing array, adding, deleting,serializing,
+//and deserializing records.
 struct RecordArray {
   Record *data_;
   std::uint32_t size_;
   std::uint32_t capacity_;
-
+  //Default constructor
   RecordArray();
-  RecordArray(std::uint32_t size, std::uint32_t capacity);
-
+  //Constructor that creates a RecordArray with the given size and capacity
+  //Size is current elements, capacity is max elements
+  explicit RecordArray(std::uint32_t size, std::uint32_t capacity);
+  //Destructor
   ~RecordArray();
 
-  Record &operator[](std::uint32_t i) { return data_[i]; }
-
-  const Record &operator[](std::uint32_t i) const { return data_[i]; }
-
+  //Resize array to new capacity
   void resize(std::uint32_t);
-
+  //Append a new record
   void add_record(Record);
-
+  //Delete a record
   void delete_record(std::uint32_t index);
+  //Writes to a binary output stream
+  void serialize(std::ostream& out) const;
+  //Reads a record from a binary output stream
+  void deserialize(std::istream& in);
 };
