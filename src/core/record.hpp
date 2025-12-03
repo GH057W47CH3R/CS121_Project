6a71938d76799297d0ed93b5bb1f49e2e045672b
Kaleb <kchank02@gmail.com>
2025-12-02 20:35:29 -0600

    Added address and phone member variables

--- src/core/record.hpp
+++ src/core/record.hpp
@@ -1,21 +1,31 @@
 #pragma once
 #include <cstddef>
+#include <fstream>
+#include <cstring>
 
 // We will add to this
 struct Record {
   inline static constexpr std::size_t max_name_bytes = 64;
+  inline static constexpr std::size_t max_address_bytes = 128;
+  inline static constexpr std::size_t max_phone_bytes   = 32;
+
+  std::uint32_t id;
 
   char name[max_name_bytes];
+  char address[max_address_bytes];
+  char phone[max_phone_bytes];
 
-  const char *get_name() const { return name; }
-
-  //Serialize the record by writing the raw bytes to an output stream
-  void serialize(std::ostream& out) const{
-    out.write(name, max_name_bytes);
-  }
-  //Deserialize the record by reading the raw bytes from an input stream
-  void deserialize(std::istream& in) {
-    in.read(name, max_name_bytes);
+//Serialize the record by writing the raw bytes to an output stream
+void serialize(std::ostream& out) const{
+  out.write(name, max_name_bytes);
+  out.write(address, max_address_bytes);
+  out.write(phone, max_phone_bytes);
+}
+//Deserialize the record by reading the raw bytes from an input stream
+void deserialize(std::istream& in) {
+  in.read(name, max_name_bytes);
+  in.read(address, max_address_bytes);
+  in.read(phone, max_phone_bytes);
 }
 
 };
